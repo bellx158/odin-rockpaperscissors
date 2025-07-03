943e5248a377cbcb6e578cdc227a88f39b6b1ac1
Isabella E <ie70431n@pace.edu>
2025-07-02 21:33:33 -0400

    added single round logic

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+   
+  let humanScore = 0;
+  let computerScore = 0;
 
 function getComputerChoice(){
     const randomNumber = Math.floor(Math.random() * 3); // Generates a random integer (0, 1, or 2)
@@ -24,5 +27,36 @@ function getHumanChoice(){
            }
     }
  console.log(getHumanChoice());
-    
-  
+
+
+  function playRound(humanChoice, computerChoice){
+
+    const normalizedHumanChoice = humanChoice.toLowerCase();
+
+    let roundResult = ""; 
+
+    if (normalizedHumanChoice === computerChoice) {
+        roundResult = `It's a tie! Both chose ${normalizedHumanChoice}.`;
+    }
+    // Win conditions
+    else if (
+        (normalizedHumanChoice === "rock" && computerChoice === "scissors") ||
+        (normalizedHumanChoice === "paper" && computerChoice === "rock") ||
+        (normalizedHumanChoice === "scissors" && computerChoice === "paper")
+    ) {
+        humanScore++;
+        roundResult = `You win! ${normalizedHumanChoice} beats ${computerChoice}.`;
+    }
+    //Computer win conditions
+    else {
+        computerScore++;
+        roundResult = `You lose! ${computerChoice} beats ${normalizedHumanChoice}.`;
+    }
+
+    console.log(roundResult);
+    console.log(`Current Score: Human ${humanScore} - Computer ${computerScore}`);
+
+   
+}
+
+
